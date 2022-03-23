ddada4f46de166361e880aacb22b671764f81cd4
zishu.ma <zishu.ma@thoughtworks.com>
2022-03-23 10:42:40 +0800

    Initial Commit

---
+++ AssignmentOne/AssignmentOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentOneApp.swift
+//  AssignmentOne
+//
+//  Created by Zishu Ma on 2022/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
